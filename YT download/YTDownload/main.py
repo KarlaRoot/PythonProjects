edb7b0848ab3b34e24ebaf8aed3957b4a817c58a
KarlaRoot <rootkarla1@gmail.com>
2024-11-27 19:28:39 +0100

    Update main.py
    Update index.html
    
    Update main.py

--- YT download/YTDownload/main.py
+++ YT download/YTDownload/main.py
@@ -2,19 +2,20 @@ from flask import Flask, render_template, request, flash, session, make_response
 from video import Video
 """Legal and Ethical Notice
 The program YT download/YTDownload is for legal use only. Use of this program must comply with the laws of your country and YouTube's Terms of Service (Terms of Service). The author is not responsible for any illegal or unethical use of this program. Please only download content for which you have explicit permission or content that is in the public domain."""
+
 app = Flask(__name__)
 app.secret_key = "heslo"
 
 @app.route("/cookies")
 def cookies():
     response = make_response(render_template("index.html"))
-    response.set_cookie("testovací sušenka", "Ale")
+    response.set_cookie("Test cookie", "But")
     return response
 
 @app.route("/")
 @app.route("/uvod")
 def uvodniStranka():
-    susenka = request.cookies.get("testovací sušenka")
+    susenka = request.cookies.get("Test cookie")
     print(susenka)
     return render_template("index.html")
 
@@ -26,20 +27,20 @@ def result():
 
     flash("Díky za použití", "uspech")
     
-    # stáhnutí videa
-    status = video.stahnout()  # metoda pro stažení videa
-    if not status:  # stažení se nepovedlo
-        flash("Zadané URL nebylo rozpoznáno, zkontrolujte, že je ok.", "chyba")
-        return render_template("index.html", pocetStahnuti=session.get("pocetStahnuti", 0))
+    # download video
+    status = video.stahnout()  # methods for downloanding video
+    if not status:  # download failed
+        flash("The entered URL was not recognized, please check that it is ok.", "error")
+        return render_template("index.html", pocetStahnuti=session.get("pocetStahnuti", 0))# number of downloads
 
-    # úspěšné stažení
+    # successful download
     try:
         session["pocetStahnuti"] = session.get("pocetStahnuti", 0) + 1
     except Exception as e:
         print("Session error:", e)
         session["pocetStahnuti"] = 1
 
-    flash("Video bylo úspěšně staženo", "uspech")
+    flash("Video downloaded successfully", "success")
     return render_template(
         "index.html",
         odkaz=video.odkaz(),
